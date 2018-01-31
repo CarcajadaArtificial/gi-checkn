843dcf3e14cfd040bd7fd60e617f0eb59a6c0812
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2018-01-31 14:00:44 -0600

    Static Pages - Version 2.0.4

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  # Home, checkn.mx
+  def index
+
+  end
+
+  # Log In, checkn.mx/login
+  def login
+
+  end
+
+  # User Profile, checkn.mx/profile
+  def profile
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :events
+  # Static Pages
+  root to: "pages#index"
+  get 'login', to: 'pages#login', as: 'login'
+  get 'profile', to: 'pages#profile', as: 'profile'
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
