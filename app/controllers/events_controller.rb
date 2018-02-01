3f282e9c3f382d24b380872fc9993a4321e9ed41
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2018-01-31 19:21:31 -0600

    Event Views - Version 2.1.0

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,24 +1,101 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
+                                                # ======================================================
+                                                # Method: show
+                                                # URL: checkn.mx/events/:Urlname
+                                                # Alias: event_home
+                                                # Description: Display all the information of an event.
+  def show
+  end
+                                                # ======================================================
+                                                # Method: preregister
+                                                # URL: checkn.mx/events/:Urlname/preregister
+                                                # Alias: preregister
+                                                # Description: Updates the information of an assistant,
+                                                #   identified by the ticket's reference.
+  def preregister
+  end
+                                                # ======================================================
+                                                # Method: checkout
+                                                # URL: checkn.mx/events/:Urlname/checkout
+                                                # Alias: checkout
+                                                # Description: Screen that displays the ticket's barcode
+                                                #   and instructions.
+  def checkout
+  end
+                                                # ======================================================
+                                                # Method: register
+                                                # URL: checkn.mx/events/:Urlname/register
+                                                # Alias: register
+                                                # Description: Manages the attendance at the time of the
+                                                #   event. Might be registering for the event in general
+                                                #   or for an activity in particular.
+  def register
+  end
+                                                # ======================================================
+                                                # Method: sale
+                                                # URL: checkn.mx/events/:Urlname/sale
+                                                # Alias: sale
+                                                # Description: Helps the staff sell tickets. Assignes
+                                                #   email and full name to a particular ticket.
+  def sale
+  end
+                                                # ======================================================
+                                                # Method: new
+                                                # URL: checkn.mx/events/new
+                                                # Alias: event_new
+                                                # Description: Manages the attendance at the time of the
+                                                #   event. Might be registering for the event in general
+                                                #   or for an activity in particular.
+  def new
+    @event = Event.new
+  end
+                                                # ======================================================
+                                                # Method: dashboard
+                                                # URL: checkn.mx/events/:Urlname/dashboard
+                                                # Alias: dashboard
+                                                # Description: Displays generally useful information of
+                                                #   the event.
+  def dashboard
+  end
+                                                # ======================================================
+                                                # Method: manager
+                                                # URL: checkn.mx/events/:Urlname/manager
+                                                # Alias: manager
+                                                # Description: Manages the elements (ticket types,
+                                                #   questions, activities and activity types) of an
+                                                #   event.
+  def manager
+  end
+                                                # ======================================================
+                                                # Method: edit
+                                                # URL: checkn.mx/events/:Urlname/edit
+                                                # Alias: event_edit
+                                                # Description: Edits the general event information
+  def edit
+  end
+                                                # ======================================================
+                                                # Method: staff
+                                                # URL: checkn.mx/events/:Urlname/staff
+                                                # Alias: staff
+                                                # Description: Manages the staff and permissions.
+  def staff
+  end
+                                                # ======================================================
+                                                # Method: database
+                                                # URL: checkn.mx/events/:Urlname/database
+                                                # Alias: database
+                                                # Description: Filters, displays and groups the whole
+                                                #   database of tickets.
+  def database
+  end
+
+                                                # EVENT SCAFFOLD METHODS
   # GET /events
   # GET /events.json
   def index
     @events = Event.all
-  end
-
-  # GET /events/1
-  # GET /events/1.json
-  def show
-  end
-
-  # GET /events/new
-  def new
-    @event = Event.new
-  end
-
-  # GET /events/1/edit
-  def edit
   end
 
   # POST /events
--- app/views/events/_event.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! event, :id, :name, :description, :urlName, :startDate, :endDate, :imageUrl, :mainColor, :color2, :locationCode, :created_at, :updated_at
-json.url event_url(event, format: :json)
