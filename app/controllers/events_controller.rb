d1d44b9ba11554912165a75bcb3e0691f83a97e7
Daniel González <daniel.gv.1993@gmail.com>
2018-03-01 03:00:39 -0600

    Corrections to register

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -47,7 +47,7 @@ class EventsController < ApplicationController
                                                 #   or for an activity in particular.
   def register
     if current_user
-      @activities = Event.find(1).activities
+      @activities = Event.find(params[:id]).activities
       if params[:ticket]
         @ticket, @status = Ticket.register(params[:ticket], params[:activity_id], params[:id])
       end
